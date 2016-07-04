c380cae4de06910ee3d78b83337adb0a3e76ff98
Hai Feng Kao <haifeng@cocoaspice.in>
2016-07-04 17:10:32 +0800

    - disable the injection of all cookies at the beginning of page load (should do it in domain specific way)

--- YWebView/Classes/YWebView.m
+++ YWebView/Classes/YWebView.m
@@ -87,7 +87,10 @@
     WKUserContentController* controller = configuration.userContentController ?: [[WKUserContentController alloc] init];
     configuration.userContentController = controller;
 
-    [YWebView addCookieInScriptWithController:controller];
+    // TODO: addCookieInScriptWithController should not put all cookies of all domains into the javascript
+    // BUT we don't know the correct domain unless the request has been loaded, what should we do?
+    //[YWebView addCookieInScriptWithController:controller];
+    
     [YWebView addCookieOutScriptWithController:controller handler:handler]; // will add Y_HANDLER_NAME here
 
     if (self = [super initWithFrame:frame configuration:configuration]) {

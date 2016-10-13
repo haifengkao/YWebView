6e2303c89dd5c4729448aaa6fc003b6cd4bbb12f
senfi <tobias.banaszak@gmail.com>
2016-10-13 08:24:52 +0200

    NS_DESIGNATED_INITIALIZER
    Xcode warns that initwithCoder: should be implemented because it is marked as NS_DESIGNATED_INITIALIZER for WKWebView.

--- YWebView/Classes/YWebView.m
+++ YWebView/Classes/YWebView.m
@@ -74,6 +74,11 @@
     return [self initWithFrame:CGRectZero];
 }
 
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    return [self initWithCoder:coder];
+}
+
 - (instancetype)initWithFrame:(CGRect)frame
 {
     return [self initWithFrame:frame configuration:nil];

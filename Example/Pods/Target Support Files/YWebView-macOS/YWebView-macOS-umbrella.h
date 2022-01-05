2721c92326e140f2021d1d5f4c3137a5c54e8ca7
Hai Feng Kao <haifeng@cocoaspice.in>
2022-01-06 03:34:57 +0800

    bump 0.20.0

--- Example/Pods/Target Support Files/YWebView-macOS/YWebView-macOS-umbrella.h
+++ Example/Pods/Target Support Files/YWebView-macOS/YWebView-macOS-umbrella.h
@@ -10,8 +10,6 @@
 #endif
 #endif
 
-#import "NSString+Cookie.h"
-#import "YWebView.h"
 
 FOUNDATION_EXPORT double YWebViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char YWebViewVersionString[];
